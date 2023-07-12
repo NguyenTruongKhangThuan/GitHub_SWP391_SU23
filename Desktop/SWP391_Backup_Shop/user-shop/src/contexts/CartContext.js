70a1caa3224a90bb2dcf93a28547f4a3499e43bc
Nguyen Truong Khang Thuan <thuanntkse171523@fpt.edu.vn>
2023-07-12 10:52:29 +0700

    Backup First Update: User Authentication and Shop Modification

---
+++ Desktop/SWP391_Backup_Shop/user-shop/src/contexts/CartContext.js
@@ -0,0 +1,106 @@
+import React, {createContext, useEffect, useState} from 'react';
+
+export const CartContext = createContext()
+
+const CartProvider = ({children}) => {
+  const [cart, setCart] = useState([])
+
+  const [itemAmount, setItemAmount] = useState(0)
+
+  //update Item Quantity 
+  useEffect(() => {
+    if(cart)
+    {
+      const amount = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.amount
+      }, 0);
+      setItemAmount(amount);
+    }
+  }, [cart]);
+
+  //total price state
+  const [total,setTotal] = useState(0);
+
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount;
+    }, 0);
+    setTotal(total)
+  })
+
+  const addToCart = (product, id)=>{
+
+    //Part 1: Add To Cart
+    const newItem = {...product, amount: 1}
+    
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+
+    if (cartItem) {
+      const newCart = [...cart].map((item)=> {
+        if(item.id === id)
+        {
+          return {...item, amount: cartItem.amount + 1};
+        }
+        else{
+          return item;
+        }
+      });
+      setCart(newCart)
+    }
+    else {
+      setCart([...cart, newItem])
+    }
+  }
+
+  //Toggle increaments in amount
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id)
+    addToCart(cartItem, id)
+  }
+
+  //Toggle decreaments of the quantity
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item)=> {
+      return item.id === id
+    });
+    if(cartItem){
+      const newCart = cart.map(item => {
+        if(item.id === id)
+        {
+          return {...item, amount: cartItem.amount - 1}
+        }
+        else{
+          return item;
+        }
+      })
+      setCart(newCart);
+    }
+    
+      if(cartItem.amount < 2){
+        removeFromCart(id);
+      }
+    
+  }
+
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => {
+      return item.id !== id;
+    })
+    setCart(newCart)
+  }
+
+  const clearCart = () => {
+    setCart([])
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, itemAmount, total, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount}}>
+      {children}
+    </CartContext.Provider>
+  )
+  ;
+};
+
+export default CartProvider;
---
+++ Desktop/SWP391_Backup_Shop/user-shop/src/landing-page/LandingPage.js
@@ -0,0 +1,51 @@
+import './LandingPage.css';
+
+import React, { useState, useEffect } from 'react';
+
+import ForestBackground from './assets/Forest.png'
+
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Roles from './components/FeaturedCards';
+
+function App() {  
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+
+      if (prevScrollPos > currentScrollPos) {
+        setShowNavbar(true); // Scrolling up, show the navbar
+      } else {
+        setShowNavbar(false); // Scrolling down, hide the navbar
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
+  return (
+    <div className='w-[100%] overflow-hidden'>
+      <Navbar hidden={!showNavbar} />
+      <div
+        className="bg-cover bg-center h-[1000px] items-center"
+        style={{
+          backgroundImage: `url(${ForestBackground})`, // Use the imported image as background
+          marginTop: showNavbar ? 0 : '-80px',
+        }}
+      >
+        <Hero/>
+      </div>
+      <About/>
+      <Roles/>
+    </div>
+  );
+}
+
+export default App;
