9e44bd8b4d0579db482a75de0bb68821a361c2b6
HuynhNguyenThaiDuong <134895539+HuynhNguyenThaiDuong@users.noreply.github.com>
2023-07-28 11:08:01 +0700

    Add other section for gamepacks

--- SWP391_Backup_Shop/user-shop/src/pages/SpecialShop.js
+++ SWP391_Backup_Shop/user-shop/src/pages/SpecialShop.js
@@ -53,7 +53,7 @@ const SpecialShops = () => {
                 <>{getCategoryHeader(boardGame)}</>
                 <div
                   className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-4 xl: grid-cols-5 gap-[30px]
-            max-w-sm mx-auto md:max-w-none md:mx-0 bg-gradient-to-tr"
+            max-w-sm mx-auto md:max-w-none md:mx-0 "
                 >
                   {searchedProduct === null
                     ? products.map((product) => {
